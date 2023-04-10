74782f000caf6b0256cf0c75251ddf8f32e47ce3
Yasmine Hsaini <youyasmine04@gmail.com>
2023-04-10 14:08:52 -0700

    meow

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line, to stdout
+ *_puts - prints a string
  * @str: string to print
+ *
+ * Description: prints a string
+ * On success: return the number of characters printed
  */
+
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
+while (*str)
+_putchar(*str++);
+
+_putchar('\n');
 }
--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-0x09. C - Static libraries
+Introduction to C - Static libraries
